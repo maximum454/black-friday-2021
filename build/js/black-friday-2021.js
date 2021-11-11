110c65225773e322726aed7196fe27adf1a38f6b
Maxim Karachyov <karachyov.m@gmail.com>
2021-11-11 16:40:52 +0600

    fix

--- build/js/black-friday-2021.js
+++ build/js/black-friday-2021.js
@@ -4,6 +4,15 @@ $(function () {
     $('#clock').countdown('2021/11/25', function(event) {
         $(this).html(event.strftime('<div>%D <div class="sub-slog">дня</div></div><span>:</span><div>%H<div class="sub-slog">часов</div></div><span>:</span><div>%M <div class="sub-slog">минут</div></div>'));
     });
+    $('a[href^="#"]').on('click', function(event) {
+        // отменяем стандартное действие
+        event.preventDefault();
+
+        var sc = $(this).attr("href"),
+            dn = $(sc).offset().top-130;
+
+        $('html, body').animate({scrollTop: dn}, 1000);
+    });
 })
 
 const swiperBlackFriday2021 = new Swiper('.swiper-black-friday-2021', {
