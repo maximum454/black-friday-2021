7ab4ee0bb097b714fa32ed13e44a8fbe9a971910
Maxim Karachyov <karachyov.m@gmail.com>
2021-11-11 14:21:25 +0600

    fix

--- src/js/black-friday-2021.js
+++ src/js/black-friday-2021.js
@@ -1,14 +1,8 @@
 
 
 $(function () {
-    $('a[href^="#"]').on('click', function(event) {
-        // отменяем стандартное действие
-        event.preventDefault();
-
-        var sc = $(this).attr("href"),
-            dn = $(sc).offset().top-130;
-
-        $('html, body').animate({scrollTop: dn}, 1000);
+    $('#clock').countdown('2021/11/25', function(event) {
+        $(this).html(event.strftime('%D<span>:</span>%H<span>:</span>%M'));
     });
 })
 
